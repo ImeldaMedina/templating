7dd73e3f8093e76b682be18a1fedd465f7a0cc8d
Imelda Medina <imedina-castaneda@mail.greenriver.edu>
2020-01-27 13:33:22 -0500

    adding index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Imelda Medina
+ * 1/27/2020
+ * 328/templating.index.php
+ */
+session_start();
+//Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+//Require autoload file
+require("vendor/autoload.php");
+//Instantiate F3
+$f3 = Base:: instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo "<h1>Welcome!</h1>";
+//    echo $view->render('views/home.html');
+});
+
+//Run F3
+$f3->run();
